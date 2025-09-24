f03ff6274433ca918a42b7a3defb279ae2d1154e
Alfatitah Alifia Putri <alfatitahalifia@gmail.com>
2025-09-24 13:45:24 +0700

    anak 1

--- mamah.java
+++ mamah.java
@@ -1,5 +1,7 @@
 public class mamah {
     public static void main(String[] args) {
         System.out.println("Hi kidz, ini mamah");
+                System.out.println("mah ini aku anakmu");
+
     }
 }
