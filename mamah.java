479c46c072f9d7e3af18dd7f08b8750dead3f355
revalinda <revalindaph@gmail.com>
2025-09-24 13:52:31 +0700

    anak mamah papah

--- mamah.java
+++ mamah.java
@@ -2,6 +2,7 @@ public class mamah {
     public static void main(String[] args) {
         System.out.println("Hi kidz, ini mamah");
                 System.out.println("mah ini aku anakmu");
+                System.out.println("mah aku lapar");
 
     }
 }
